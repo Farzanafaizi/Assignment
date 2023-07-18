35b1a4ae8b690958dfe4359ce23744586f59282b
Farzanafaizi <farzana.faizi34@gmail.com>
2023-07-18 22:35:46 +0500

    created the new file Assignments/year.py

---
+++ Assignments/year.py
@@ -0,0 +1,13 @@
+daysInaYear = 365
+hoursInaDay = 24
+minutesInHour = 60
+secondsInMin = 60
+
+totalHoursYear = 365 * 24
+print(totalHoursYear, "hours per year")
+
+totalMinYear = 365 * 24 * 60 
+print(totalMinYear, "minutes per year")
+
+totalSecInYear = 365 * 24 * 60 * 60
+print(totalSecInYear, "seconds per year")
