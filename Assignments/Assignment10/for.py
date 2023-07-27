552c9f5af75e8ad50183293cd32a835b76813edd
Farzanafaizi <farzana.faizi34@gmail.com>
2023-07-27 10:57:50 +0500

    created if and loop files plus adjusted folders.

---
+++ Assignments/Assignment10/for.py
@@ -0,0 +1,5 @@
+numbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+for x in numbers:
+    print(x)
+
+
---
+++ Assignments/Assignment10/if.py
@@ -0,0 +1,9 @@
+a = 25    #age 20_25
+b = 19    #age 13_19
+
+if a < b:
+    print("teenager")
+elif a == b:
+    print("they are adults.")
+else:
+    print("adult")    
