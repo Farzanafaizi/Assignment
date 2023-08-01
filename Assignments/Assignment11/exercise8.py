0290963a5553b0f37aabf86cf9b3b1d456c04b8a
Farzanafaizi <farzana.faizi34@gmail.com>
2023-08-01 10:17:00 +0500

    Assignment 11th completed

--- Assignments/Assignment11/exercise8.py
+++ Assignments/Assignment11/exercise8.py
@@ -1,8 +1,9 @@
-def palindrome(a):
-
-
-
+def palindrome(s):
+    if s == s[::-1]:
+        return True
+    else:
+        return False
 
 
 print(palindrome("radar"))  
-
+print(palindrome("python"))
--- Assignments/Assignment11/exercise9.py
+++ Assignments/Assignment11/exercise9.py
@@ -1,5 +1,9 @@
 def sum_multiples(n):
+    sum = 0
+    for i in range(n):
+        if i%3==0 or i%5==0:
+            sum += i
+    return sum
 
-
-    print(sum_multiples(3, 5))
+print(sum_multiples(100))
     
